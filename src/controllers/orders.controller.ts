aee936675c57dff225f782158e52faf6d4a072ee
Ahmed Shobky <zashobky4@gmail.com>
2024-01-24 22:02:45 +0200

    'paginaiton'

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -4,15 +4,22 @@ import { fetchOrdersWithFileId } from "../services/orders.service";
 
 export async function getOrdersWithFileId(req: Request, res: Response) {
   try {
-    const { file_id } = req.query;
+    const { file_id, pageIndex } = req.query;
 
     if (!file_id) {
       return res
         .status(400)
         .json({ message: "No file_id or client_id provided" });
-    }   
-    const orders = await fetchOrdersWithFileId(file_id as string);
-    res.status(200).json({orders});
+    }
+    const result = await fetchOrdersWithFileId(
+      file_id as string,
+      Number(pageIndex)
+    );
+    const totalCount = result?.totalCount || 0;
+    const orders = result?.orders || [];
+    const totalPages = result?.totalPages || 0;
+    const currentPageIndex = result?.currentPageIndex || 0;
+    res.status(200).json({ orders, totalCount, totalPages, currentPageIndex });
   } catch (error) {
     handleError(error);
   }
--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,9 +1,24 @@
 import { Order } from "../models/order.modal";
 import { handleError } from "../utils/handleError";
 
-export const fetchOrdersWithFileId = async (file_id: string) => {
+export const fetchOrdersWithFileId = async (
+  file_id: string,
+  pageIndex: number
+) => {
+  const pageSize = 10;
   try {
-    return await Order.find({ file_id: String(file_id) });
+    const totalCount = await Order.countDocuments();
+    const totalPages = Math.ceil(totalCount / pageSize);
+    const orders = await Order.find({ file_id: String(file_id) })
+      .sort({ createdAt: "desc" }) // Order by creation date
+      .skip(pageIndex * pageSize)
+      .limit(pageSize);
+    return {
+      orders,
+      totalCount,
+      totalPages,
+      currentPageIndex: pageIndex,
+    };
   } catch (err) {
     handleError(err);
   }
